3e98997ce2590d26cf8625eb1ff1a58aaf566f78
thomplth <29663345+thomplth@users.noreply.github.com>
2021-10-21 17:00:33 +0800

    Ignore IDE configs

--- src/Metric.py
+++ src/Metric.py
@@ -1,3 +1,5 @@
+import pickle
+
 class Metric():
     def chord_segmentation_accuracy(self, prediction, target):
         pass
@@ -5,6 +7,46 @@ class Metric():
     def key_segmentation_accuracy(self, prediction, target):
         pass
 
+    def get_target(self, filename):
+        input_path = '../data/data_answer' + '/' + filename + '.pydata'
+        with open(input_path, 'rb') as f:
+            return pickle.load(f)
+
+
 if __name__ == '__main__':
     metric = Metric()
     # print(metric.chord_segmentation_accuracy())
+
+
+'''
+>>> music21.music21.converter.subConverters.ConverterMusicXML
+def parseFile(self, fp: Union[str, pathlib.Path], number=None):
+    # Open from a file path; check to see if there is a pickled
+    # version available and up to date; if so, open that, otherwise
+    # open source.
+    
+    # return fp to load, if pickle needs to be written, fp pickle
+    # this should be able to work on a .mxl file, as all we are doing
+    # here is seeing which is more recent
+    from music21 import converter
+    from music21.musicxml import xmlToM21
+
+    c = xmlToM21.MusicXMLImporter()
+
+    # here, we can see if this is a mxl or similar archive
+    arch = converter.ArchiveManager(fp)
+    if arch.isArchive():
+        archData = arch.getData()
+        c.xmlText = archData
+        c.parseXMLText()
+    else:  # its a file path or a raw musicxml string
+        c.readFile(fp)
+
+    # movement titles can be stored in more than one place in musicxml
+    # manually insert file name as a movementName title if no titles are defined
+    if c.stream.metadata.movementName is None:
+        junk, fn = os.path.split(fp)
+        c.stream.metadata.movementName = fn  # this should become a Path
+    self.stream = c.stream
+    
+'''
