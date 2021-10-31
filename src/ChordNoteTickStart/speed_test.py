eafa5c8afddb52411382af3c72842a7d509ebcea
Thomas Chan <thomaschanpy@gmail.com>
2021-10-31 21:55:12 +0800

    Chord segment in chordify

--- src/ChordNoteTickStart/speed_test.py
+++ src/ChordNoteTickStart/speed_test.py
@@ -2,33 +2,41 @@ from utility import note_input_convertor
 import music21
 import time
 
-base_notes = ["C", "C#", "D-", "F", "G-", "A#", "B--", "B", "E-", "F##"]
+base_notes = ["C", "C#", "C-", "C##", "D", "D#", "D-", "D--"]
+base_notes.extend(["E", "E#", "E-", "E--", "F", "F#", "F-", "F##"])
+base_notes.extend(["G", "G#", "G-", "G--", "A", "A#", "A-", "A##"])
+base_notes.extend(["B", "B#", "B-", "B--", "B##"])
 m21_notes = [music21.note.Note(base + "5") for base in base_notes]
 base_notes_len = len(base_notes)
-intervals = ["P1", "M2", "m3", "d4", "P5", "m6", "d7"]
+intervals = ["P1", "A1", "d8", "M2", "A2", "m2", "d2", "M3", "A3", "m3", "d3"]
+intervals.extend(["P4", "A4", "d4", "P5", "A5", "d5"])
+intervals.extend(["M6", "A6", "m6", "d6", "M7", "A7", "m7", "d7"])
 intervals_len = len(intervals)
-TRIAL = 10000
+# print(base_notes_len, intervals_len)
+TRIAL = 100000
 
 if __name__ == "__main__":
 
     def use_music21_interval_test():
         start_time1 = time.time()
+        result = []
         for i in range(TRIAL):
             note = base_notes[i % base_notes_len]
             interval = intervals[i % intervals_len]
             new_note = music21.pitch.Pitch(note).transpose(interval)
-            if i < 10:
-                print(new_note.name)
+            result.append(new_note.name)
 
         # Time calculation
         print(
             "--- With using music21 library: Used %s seconds ---"
             % (time.time() - start_time1)
         )
+        return result
 
     def no_music21_library_interval_test():
         # Time calculation
         start_time = time.time()
+        result = []
 
         for i in range(TRIAL):
             # note = note_input_convertor(base_notes[i % base_notes_len])
@@ -36,14 +44,17 @@ if __name__ == "__main__":
             note = note_input_convertor(m21_note.name)
             interval = intervals[i % intervals_len]
             new_note = note.get_note_by_interval(interval)
-            if i < 10:
-                print(music21.note.Note(new_note.note_str(False)))
+            result.append(new_note.note_str(False))
 
         # Time calculation
         print(
             "--- Without using music21 library: Used %s seconds ---"
             % (time.time() - start_time)
         )
+        return result
 
-    use_music21_interval_test()
-    no_music21_library_interval_test()
+    result1 = use_music21_interval_test()
+    result2 = no_music21_library_interval_test()
+    for idx in range(TRIAL):
+        if not (result1[idx] == result2[idx]):
+            print("Error: ", result1[idx], result2[idx])
--- src/naive_segmentation.py
+++ src/naive_segmentation.py
@@ -6,7 +6,8 @@ import music21
 # TODO: transfer the lyrics from the old file to the new file
 # filename = "Nocturne_in_E_Major"
 # filename = "Chopin_F._Nocturne_in_E_Major,_Op.26_No.2"
-stream = load_file("../Gymnopdie_n1_-_E._Satie.mxl")
+# stream = load_file("../Gymnopdie_n1_-_E._Satie.mxl")
+stream = load_file("../Minuet_in_F_C.mxl")
 # stream = load_file("../data/" + filename + ".mxl")
 
 chordify_stream = chordify(stream)
@@ -16,19 +17,20 @@ time_signature = get_initial_time_signature(chordify_stream)
 print(time_signature.numerator)
 
 
-for element in stream.recurse().getElementsByClass("Measure"):
+for element in chordify_stream.recurse().getElementsByClass("Measure"):
     element.show("text")
-    notes = element.recurse().getElementsByClass("Note")
-    if len(notes) > 0:
+    chords = element.recurse().getElementsByClass("Chord")
+    if len(chords) > 0:
         # notes[0].addLyric(str("x"))
-        for note in notes:
-            mark = int(note.offset) - note.offset % time_signature.numerator == 0
+        for chord in chords:
+            mark = int(chord.offset) - chord.offset % time_signature.numerator == 0
             # mark = mark and int(note.offset) % time_signature.numerator
             if mark:
-                note.addLyric(str("x"))
+                chord.addLyric(str("x"))
 
     print("----")
 
 # export_file(stream, "../result/Minuet_in_F_C_test")
 # export_file(stream, "../result/anonymous_Twinkle_Twinkle_test")
-export_file(stream, "../result/test1")
+
+export_file(chordify_stream, "../result/test2")
