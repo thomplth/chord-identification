5b21ffab7004ee8cfb587ab8eed69e8f6675fbbe
Thomas Chan <thomaschanpy@gmail.com>
2021-11-05 13:34:11 +0800

    add data modify for data corrupted correction

---
+++ src/data_modify.py
@@ -0,0 +1,25 @@
+from Music21Utils.music21_utility import *
+
+
+# TODO: transfer the lyrics from the old file to the new file
+filename = "Nocturne_in_E_Major"
+old_stream = load_file("../data/" + filename + ".mxl")
+# filename = "Chopin_F._Nocturne_in_E_Major,_Op.26_No.2"
+# new_stream = load_file("../data/" + filename + ".mxl")
+# print(len(get_measures(chordify(old_stream))))
+
+target = old_stream.recurse().measures(70, 82)
+target.show()
+# for measure in target.recurse().getElementsByClass("Measure"):
+#     print(measure)
+#     chords = measure.recurse().getElementsByClass("Chord")
+#     if len(chords) > 0:
+#         for c in chords:
+#             if len(c.lyrics) > 0:
+#                 print(c.lyrics, c.offset)
+#     notes = measure.recurse().getElementsByClass("Note")
+#     if len(notes) > 0:
+#         for c in notes:
+#             if len(c.lyrics) > 0:
+#                 print(c.lyrics, c.offset)
+#     print("---")
