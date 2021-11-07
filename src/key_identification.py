0fc3ead0dc2f403d9aee0ecf8c17898fdbe2c727
Thomas Chan <thomaschanpy@gmail.com>
2021-11-08 00:01:30 +0800

    key identification version 0.1

--- src/key_identification.py
+++ src/key_identification.py
@@ -1,9 +1,72 @@
 import numpy as np
 from collections import Counter
 
-# TODO: Key finding algo.
+
+def select_key_profile(profile="Temperley"):
+    if profile == "Krumhansl-Kessler":
+        return np.array(
+            [6.35, 2.23, 3.48, 2.33, 4.38, 4.09, 2.52, 5.19, 2.39, 3.66, 2.29, 2.88]
+        ), np.array(
+            [6.33, 2.68, 3.52, 5.38, 2.60, 3.53, 2.54, 4.75, 3.98, 2.69, 3.34, 3.17]
+        )
+    elif profile == "Temperley":
+        return (
+            np.array([5.0, 2.0, 3.5, 2.0, 4.5, 4.0, 2.0, 4.5, 2.0, 3.5, 1.5, 4.0]),
+            np.array([5.0, 2.0, 3.5, 4.5, 2.0, 4.0, 2.0, 4.5, 3.5, 2.0, 1.5, 4.0]),
+        )
+    else:  # Longuet-Higgins/ Steedman a.k.a. flatten key profile
+        return (
+            np.array([1.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0]),
+            np.array([1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.5, 0.5]),
+        )
+
+
+# Key finding algo.
 def find_key(measure_chromagram):
-    return "C"
+    major_profile, minor_profile = select_key_profile()
+    # max_3_indices = np.argpartition(measure_chromagram, -3)[-3:]
+
+    mean_chromagram = np.mean(measure_chromagram)
+
+    def get_correlation_values(isMajor, isFast=False):
+        scale_profile = major_profile if isMajor else minor_profile
+        mean_scale = np.mean(scale_profile)
+        value_scales = []
+
+        coverage = []
+        # if efficiency is a concern, we only do max of 9 instead of 12
+        if isFast:
+            # choose C, E (Eb for minor), G
+            # pitches_class = list(max_3_indices)
+            # coverage = pitches_class + [(pitch + 7) % 12 for pitch in pitches_class]
+            # if isMajor:
+            #     coverage = coverage + [(pitch + 4) % 12 for pitch in pitches_class]
+            # else:
+            #     coverage = coverage + [(pitch + 3) % 12 for pitch in pitches_class]
+            # coverage = list(set(coverage))
+            pass
+        else:
+            coverage = range(12)
+
+        for idx in coverage:
+            rolled_chromagram = np.roll(measure_chromagram, -1 * idx)
+            correlation_value_numerator = np.sum(
+                (rolled_chromagram - mean_chromagram) * (scale_profile - mean_scale)
+            )
+            correlation_value_denominator = np.sqrt(
+                np.sum((rolled_chromagram - mean_chromagram) ** 2)
+                * np.sum((scale_profile - mean_scale) ** 2)
+            )
+            correlation_value = (
+                correlation_value_numerator / correlation_value_denominator
+            )
+            value_scales.append((correlation_value, idx, isMajor))
+        return value_scales
+
+    possible_key = max(
+        get_correlation_values(True) + get_correlation_values(False), key=lambda i: i[0]
+    )[1:]
+    return possible_key
 
 
 # determine the key of the measures in 3 layers
@@ -12,7 +75,7 @@ def determine_key(measures_dictionary):
     queue = []
     measures_possible_key = {}
     two_measures_chroma = np.zeros(12)
-    four_measures_chroma = np.zeros(12)
+    # four_measures_chroma = np.zeros(12)
 
     # identification of keys
     for idx, chroma in measures_dictionary.items():
@@ -23,22 +86,23 @@ def determine_key(measures_dictionary):
         measures_possible_key[idx] = [key_1_measure]
 
         two_measures_chroma += chroma
-        four_measures_chroma += chroma
+        # four_measures_chroma += chroma
 
         # Determine key within TWO measures
-        if len(queue) > 1:
+        if len(queue) == 2:
             key_2_measure = find_key(two_measures_chroma)
-            for i in queue[-2:]:
+            for i in queue:
                 measures_possible_key[i].append(key_2_measure)
-            two_measures_chroma -= measures_dictionary[queue[0]]
+            old_measure_idx = queue.pop(0)
+            two_measures_chroma -= measures_dictionary[old_measure_idx]
 
         # Determine key within FOUR measures
-        if len(queue) == 4:
-            key_4_measure = find_key(four_measures_chroma)
-            for i in queue:
-                measures_possible_key[i].append(key_4_measure)
-            old_measure_idx = queue.pop(0)
-            four_measures_chroma -= measures_dictionary[old_measure_idx]
+        # if len(queue) == 4:
+        #     key_4_measure = find_key(four_measures_chroma)
+        #     for i in queue:
+        #         measures_possible_key[i].append(key_4_measure)
+        #     old_measure_idx = queue.pop(0)
+        #     four_measures_chroma -= measures_dictionary[old_measure_idx]
 
     # selection of key
     measures_key = {}
@@ -46,5 +110,4 @@ def determine_key(measures_dictionary):
         occurrence_count = Counter(keys)
         # Here assume if 2 keys having same count, choose key_1_measure -> key_2_measure -> key_4_measure
         measures_key[idx] = occurrence_count.most_common(1)[0][0]
-
     return measures_key
--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ start_time = time.time()
 # stream = load_file("../Gymnopdie_n1_-_E._Satie.mxl")
 # stream = load_file("../Minuet_in_F_C.mxl")
 filename = "anonymous_Twinkle_Twinkle"
+# filename = "Chopin_F._Nocturne_in_E_Minor,_Op.72_No.1"
 stream = load_file("../data/" + filename + ".mxl")
 
 chordify_stream = chordify(stream)
@@ -23,20 +24,21 @@ scale_name = (
     if key_signature.type == "minor"
     else key_signature.tonic.name.upper()
 )
-print(scale_name)
+print("Assume all measures are in ", scale_name)
 measures_key = determine_key(key_segmentation(stream))
-print(measures_key)
+for k, v in measures_key.items():
+    print(k, v)
 
 
 if False:
     segments = uniform_segmentation(chordify_stream, time_signature)
     combined_segments = merge_chord_segment(segments)
-    for segment in combined_segments:
-        # print(segment[0], [note for note in segment[1]], notes_variation(segment[1]))
+    # print(combined_segments)
+    for segment in combined_segments[:2]:
         notes = [note_input_convertor(note) for note in segment[1]]
         result = find_chords(notes)
-        print(">>>", segment[0])
-        print_chords_names(notes, result, scale_name)
+        print(">>>", segment[0], segment[1], notes_variation(segment[1]))
+        print_chords_names(notes, result, "")
 
 
 # export_file(stream, "../result/Minuet_in_F_C_test")
