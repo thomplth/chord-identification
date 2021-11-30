7f2a566320889617ac6839542036c7aacf2489e2
Thomas Chan <thomaschanpy@gmail.com>
2021-12-01 03:13:21 +0800

    new theta result

--- src/main.py
+++ src/main.py
@@ -66,71 +66,38 @@ def main():
             flatten_stream = flatten(stream)
 
             time_signature = get_initial_time_signature(flatten_stream)
-            # print(time_signature)
-            # key_signature = get_initial_key_signature(flatten_stream)
-            # initial_scale = Scale(key_signature.tonic.name)
-            # print("Assume all measures are in ", scale_name)
+            # key_signature = get_initial_key_signature(flatten_stream) # Scale(key_signature.tonic.name)
 
-            if KeyThenChordMode:
-                # def get_measures_key():
-                measures_key = determine_key_by_adjacent(key_segmentation(stream))
-                # measures_key = determine_key_solo(key_segmentation(stream))
-                # return measures_key
+            measures_key = determine_key_by_adjacent(key_segmentation(stream))
+            # measures_key = determine_key_solo(key_segmentation(stream))
 
-                keys = measures_key
+            keys = measures_key
 
-                # def get_beats_chord():
-                # notes_in_measures = get_notes_in_measures(stream)
-                notes_in_measures = get_notes_in_measures(chordify_stream)
-                segments = uniform_segmentation(notes_in_measures, time_signature)
-                combined_segments = merge_chord_segment(segments)
+            notes_in_measures = get_notes_in_measures(chordify_stream)
+            segments = uniform_segmentation(notes_in_measures, time_signature)
+            combined_segments = merge_chord_segment(segments)
 
-                res = []
-                for segment in combined_segments:
-                    notes_frequencies = [
-                        (note_input_convertor(note_name), v)
-                        for note_name, v in segment[1].items()
-                    ]
+            res = []
+            for segment in combined_segments:
+                notes_frequencies = [
+                    (note_input_convertor(note_name), v)
+                    for note_name, v in segment[1].items()
+                ]
+                if KeyThenChordMode:
                     key_choices = find_scale_in_chord_segment(keys, segment)
                     possible_key = max(key_choices, key=key_choices.get)
                     res.append(
                         (segment[0], find_chords(notes_frequencies, possible_key))
                     )
-                    # print(res)
-                    # return res
+                else:
+                    res.append((segment[0], find_chords(notes_frequencies)))
+            # print(res)
 
-                chords = res
+            chords = res
+            if KeyThenChordMode:
                 keys = None
-                result = determine_chord(keys=keys, chords=chords)
-                export_csv(result, "result", score_file.removesuffix(".mxl"))
-
-            else:
-
-                def get_measures_key():
-                    measures_key = determine_key_by_adjacent(key_segmentation(stream))
-                    # measures_key = determine_key_solo(key_segmentation(stream))
-                    return measures_key
-
-                keys = get_measures_key()
-
-                def get_beats_chord():
-                    # notes_in_measures = get_notes_in_measures(stream)
-                    notes_in_measures = get_notes_in_measures(chordify_stream)
-                    segments = uniform_segmentation(notes_in_measures, time_signature)
-                    combined_segments = merge_chord_segment(segments)
-
-                    res = []
-                    for segment in combined_segments:
-                        notes_frequencies = [
-                            (note_input_convertor(note_name), v)
-                            for note_name, v in segment[1].items()
-                        ]
-                        res.append((segment[0], find_chords(notes_frequencies)))
-                    return res
-
-                chords = get_beats_chord()
-                result = determine_chord(keys, chords)
-                export_csv(result, "result", score_file.removesuffix(".mxl"))
+            result = determine_chord(keys=keys, chords=chords)
+            export_csv(result, "result", score_file.removesuffix(".mxl"))
 
         except Exception as error:
             print(error)
