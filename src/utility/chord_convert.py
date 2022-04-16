17328a34ab4f545b6846b8a92e86e088f2a7f0d4
Thomas Chan <thomaschanpy@gmail.com>
2022-04-16 21:11:51 +0200

    chord convert refinement

--- src/utility/chord_convert.py
+++ src/utility/chord_convert.py
@@ -6,13 +6,11 @@ if __name__ == "__main__":
 
 from chord_constant import MAJOR_CHORD_DICT, MINOR_CHORD_DICT
 from preprocess.note import Note
+from utility import get_files
 import csv
-
 import sys
 
 # Given the scale and the chord, output the notes of the chord
-
-
 def pick_chord(tonic, chord, is_major):
     chord_intervals = MAJOR_CHORD_DICT[chord] if is_major else MINOR_CHORD_DICT[chord]
     base_note = tonic
@@ -27,167 +25,68 @@ def pick_chord(tonic, chord, is_major):
     return chord_notes
 
 
-def get_files(filename=None):
-    all_scores = [f for f in os.listdir(
-        '../../data/ground_truth') if f.endswith(".csv")]
-    # print(all_scores)
-    if filename in all_scores:
-        return [filename]
-    return all_scores
-
-ENHAR = {
-    'B#': 'C',
-    'C-': 'B',
-    'D-': 'C#',
-    'E-': 'D#',
-    'E#': 'F',
-    'F-': 'E',
-    'G-': 'F#',
-    'A-': 'G#',
-    'B-': 'A#'
-}
-
 if __name__ == "__main__":
-    inpath = '../../data/ground_truth' + '/'
-    outpath = '../../data/chord/' + 'KYDataset/'
-    files = get_files()
-    
+    inpath = "../../data/ground_truth/KYDataset/"
+    outpath = "../../data/chord/KYDataset/"
+    files = get_files(inpath, ".csv")
+
+    print(len(files))
+
+    def export_chord_chroma(filename, inpath, outpath):
+        print(">>>", filename)
+        infile = open(inpath + filename, "r")
+        outfile = open(outpath + filename, "w", newline="")
+        reader = csv.reader(infile)
+        writer = csv.writer(outfile)
+
+        offset = 0.0
+        tonic, chord, is_major = "?", "?", "?"
+        notecount = {n: 0.0 for n in range(12)}
+        chroma = [0.0 for _ in range(12)]
+
+        headers = next(reader)
+        writer.writerow(
+            ("offset", "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b")
+        )
+        for row in reader:
+            try:
+                new_offset = float(row[0])
+            except ValueError:
+                num, den = row[0].split("/")
+                new_offset = float(num) / float(den)
+
+            while new_offset > offset + 1.0:
+                offset += 1
+                writer.writerow(tuple([offset] + chroma))
+
+            counter = notecount.copy()
+
+            # print(row)
+            offset = new_offset
+            tonic, chord, is_major = Note(input_str=row[1]), row[3], row[2]
+
+            try:
+                notes = pick_chord(tonic, chord, is_major == "M")
+                for note in notes:
+                    pitch_class = note.get_pitch_class()
+                    counter[pitch_class] += 1.0
+                chroma = list(counter.values())
+            except KeyError:
+                print(">>> ERROR <<<")
+                print(row)
+
+            writer.writerow(tuple([offset] + chroma))
+
+        infile.close()
+        outfile.close()
+
+    for filename in files:
+        export_chord_chroma(filename, inpath, outpath)
+
+    inpath = "../../data/ground_truth/Schubert/"
+    outpath = "../../data/chord/Schubert/"
+    files = get_files(inpath, ".csv")
+
     print(len(files))
     for filename in files:
-        print('>>>', filename)
-
-        infile = open(inpath + filename, 'r')
-        outfile = open(outpath + filename, 'w', newline='')
-        reader = csv.reader(infile)
-        writer = csv.writer(outfile)
-
-        offset = 0.0
-        tonic, chord, is_major = '?', '?', '?'
-        notecount = {n: 0.0 for n in ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']}
-        chroma = [0.0 for _ in range(12)]
-
-        headers = next(reader)
-        writer.writerow(('offset', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b'))
-        for row in reader:
-            try:
-                new_offset = float(row[0])
-            except ValueError:
-                num, den = row[0].split( '/' )
-                new_offset = float(num) / float(den)
-
-            while new_offset > offset + 1.0:
-                offset += 1
-                writer.writerow(tuple([offset] + chroma))
-
-            counter = notecount.copy()
-
-            # print(row)
-            offset = new_offset
-            tonic, chord, is_major = Note(input_str=row[1]), row[3], row[2]
-
-            try:
-                notes = pick_chord(tonic, chord, is_major == 'M')
-
-                for note in notes:
-                    s = note.__str__()
-                    if s not in counter:
-                        if s not in ENHAR:
-                            print('>>>', s)
-
-                        s = ENHAR[s]
-                    counter[s] += 1.0
-
-                chroma = list(counter.values())
-                # print(['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'])
-                # print(chroma)
-                # print([note.__str__() for note in notes])
-            except KeyError:
-                print(">>> ERROR <<<")
-                print(row)
-
-            writer.writerow(tuple([offset] + chroma))
-
-        infile.close()
-        outfile.close()
-
-'''
-def get_files(filename=None):
-    all_scores = [f for f in os.listdir(
-        '../../data/ground_truth_Schubert') if f.endswith(".csv")]
-    # print(all_scores)
-    if filename in all_scores:
-        return [filename]
-    return all_scores
-
-ENHAR = {
-    'B#': 'C',
-    'C-': 'B',
-    'D-': 'C#',
-    'E-': 'D#',
-    'E#': 'F',
-    'F-': 'E',
-    'G-': 'F#',
-    'A-': 'G#',
-    'B-': 'A#'
-}
-
-if __name__ == "__main__":
-    inpath = '../../data/ground_truth' + '_Schubert/'
-    outpath = '../../data/chord/' + 'Schubert/'
-    for filename in get_files():
-        print('>>>', filename)
-
-        infile = open(inpath + filename, 'r')
-        outfile = open(outpath + filename, 'w', newline='')
-        reader = csv.reader(infile)
-        writer = csv.writer(outfile)
-
-        offset = 0.0
-        tonic, chord, is_major = '?', '?', '?'
-        notecount = {n: 0.0 for n in ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']}
-        chroma = [0.0 for _ in range(12)]
-
-        headers = next(reader)
-        writer.writerow(('offset', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b'))
-        for row in reader:
-            try:
-                new_offset = float(row[0])
-            except ValueError:
-                num, den = row[0].split( '/' )
-                new_offset = float(num) / float(den)
-
-            while new_offset > offset + 1.0:
-                offset += 1
-                writer.writerow(tuple([offset] + chroma))
-
-            counter = notecount.copy()
-
-            # print(row)
-            offset = new_offset
-            tonic, chord, is_major = Note(input_str=row[1]), row[3], row[2]
-
-            try:
-                notes = pick_chord(tonic, chord, is_major == 'M')
-
-                for note in notes:
-                    s = note.__str__()
-                    if s not in counter:
-                        if s not in ENHAR:
-                            print('>>>', s)
-
-                        s = ENHAR[s]
-                    counter[s] += 1.0
-
-                chroma = list(counter.values())
-                # print(['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'])
-                # print(chroma)
-                # print([note.__str__() for note in notes])
-            except KeyError:
-                print(">>> ERROR <<<")
-                print(row)
-
-            writer.writerow(tuple([offset] + chroma))
-
-        infile.close()
-        outfile.close()
-'''
+        export_chord_chroma(filename, inpath, outpath)
